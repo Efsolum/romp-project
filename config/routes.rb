4b184e4f7cd3f70360429b1d4867fc96dd0afcf4
Matthew O'N.S Jordan <matthewjordannm@gmail.com>
2015-09-05 11:51:32 -0400

    Updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :dashboard do
+    get 'index'
+    get 'about'
+    get 'contact'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
