9c825d9d4b327536cd605bfdce2eca8112cbbf2b
Matthew O'N.S Jordan <matthewjordannm@gmail.com>
2015-09-23 18:05:02 -0400

    Added icons for logging in and out of the site. Also added a link to the user login page. In addition relavent methods have been added.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :loggedin?, :current_user
 
   protected
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def loggedin?
+    current_user
+    defined? @current_user
+  end
+
   def authenticate
+    current_user
     unless @current_user
       redirect_to new_session_url,
         alert: 'You need to sign in or sign up before continuing.'
