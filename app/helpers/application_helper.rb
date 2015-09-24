e23c285e768885ee11e9fec05b06920776af60e7
Matthew O'N.S Jordan <matthewjordannm@gmail.com>
2015-09-24 09:56:42 -0400

    Added an icon and image helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image_asset(image, options = {})
+    image_tag("/assets/images/" + image, options)
+  end
+
+  def icon_asset(icon, options = {})
+    image_tag("/assets/icons/" + icon, options)
+  end
 end
