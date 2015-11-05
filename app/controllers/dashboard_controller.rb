b8aa68ca8a9a1a64a6235cc4b747a1f7d0976775
Matthew O'N.S Jordan <matthewjordannm@gmail.com>
2015-11-05 12:31:53 -0400

    Updated the homepage.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
 class DashboardController < ApplicationController
   before_action :authenticate, only: [:admin]
 
   def index
-    @content = 'Welcome to Romp International'
+    @title = 'Welcome To Romp International'
+    @content = '
+In only (3) three years Romp International (Romp) has become one of Barbados most improved small bands on the road for 2015. Romp is known for being the hypest bands on the road for Kadooment, this reputation wasn’t built overnight; it takes time and patience. Romp also caterers to all sizes by making costumes to compliment the full figured woman while making her look and feel sexy.
+The extraordinary history to Romp is due to the founder for her passion and commitment to this band. This success is also partly due to her ability to listen to people, observe and react to current trends and anticipate the wishes of her customers. This band has survived through the economic downturn in the island and continues to be innovators overcoming obstacles. It is thanks to the value placed on the men and women who come together to make this organization great.'
   end
 
   def about
